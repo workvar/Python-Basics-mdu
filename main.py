211b8389e162ed9feaa16a46e4a4b62473ce0f55
maliksjyoti88535-ai <maliksjyoti88535@gmail.com>
2026-08-10 22:48:05 -1200

    fix error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ numbers = [10, 15, 20, 25, 30]
 total = 0
 
 for i in range(0, len(numbers)):
-    if numbers[i] % 2 = 0:
+    if numbers[i] % 2 == 0:
         print("Even number:", numbers[i])
         total += numbers[i]
     else:
@@ -12,9 +12,9 @@ print("Total of even numbers:", total)
 
 count = 0
 while count < len(numbers):
-    if numbers[count] > 20
+    if numbers[count] > 20:
         print("Large:", numbers[count])
     count = count + 2
 
 for num in numbers:
-    print("Square:", num ** 2
+    print("Square:", num ** 2)
