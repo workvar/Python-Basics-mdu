06a0ea3b8c825e0dd75a9f1a6fb13e6794cd74ed
Khushi Saxena <saxenakhushi891@gmail.com>
2026-08-12 16:13:38 +0530

    tabulate completed

--- api/Khushi/api.py
+++ api/Khushi/api.py
@@ -1,4 +1,5 @@
 import requests
+from tabulate import tabulate
 url = "https://wttr.in/Dehradun?format=j1"
 response = requests.get(url)
 
@@ -8,11 +9,36 @@ weather_data = response.json()
 # print("Weather data:", weather_data)
 # or print(response.json())
 
+#  Current Weather
 current_weather = weather_data["current_condition"][0]
 temperature = current_weather["temp_C"]
 condition = current_weather["weatherDesc"][0]["value"]
 
-print("\n--- Dehradun Weather ---")
-print("City:", "Dehradun")
-print("Temperature:", temperature, "°C")
-print("Condition:", condition)
+#  Nearest Area
+nearest_area = weather_data["nearest_area"][0]
+
+area = nearest_area["areaName"][0]["value"]
+country = nearest_area["country"][0]["value"]
+region= nearest_area["region"][0]["value"]
+
+# Weather / Chance of rain
+weather = weather_data["weather"][0]
+hourly= weather["hourly"]
+
+chance_of_rain = hourly[0]["chanceofrain"]
+
+# Table
+table=[
+    ["current_condition","temp_c",temperature],
+    ["current_condition","weatherDesc",condition],
+    ["nearest_area","areaName",area],
+    ["nearest_area","country",country],
+    ["nearest_area","region",region],
+    ["weather","chanceOfRain",chance_of_rain]
+]
+
+print("\n--- Dehradun Weather Data ---")
+print(
+    tabulate(table, headers=["Category", "Information", "Value"],
+             tablefmt="grid")
+)
