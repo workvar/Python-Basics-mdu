228d32703f3403097afc11c72d5ea0095119b74c
molikprabhakar <moliksharma123@gmail.com>
2026-08-13 13:46:38 +0530

    Weather info

--- api/Molik/API.py
+++ api/Molik/API.py
@@ -1,9 +1,9 @@
 import requests
 
-
-url = " https://wttr.in/Dehradun?format=j1"
+url = "https://wttr.in/Dehradun?format=j1"
 
 response = requests.get(url)
 
 data = response.json()
-print (data)
+
+print(data)
