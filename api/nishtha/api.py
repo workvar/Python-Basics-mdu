5bf61d1bf318d16466e6d9dc3d837831c9295232
nistha144 <nisthakantura@gmail.com>
2026-08-14 10:15:29 +0530

    Add Nishtha weather API

---
+++ api/nishtha/api.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://wttr.in/Dehradun?format=j1"
+
+response = requests.get(url)
+data = response.json()
+
+current = data["current_condition"][0]
+area = data["nearest_area"][0]
+weather = data["weather"][0]
+
+print("Weather Information")
+print("-------------------")
+
+print("City:", area["areaName"][0]["value"])
+print("Country:", area["country"][0]["value"])
+print("Region:", area["region"][0]["value"])
+print("Temperature:", current["temp_C"], "°C")
+print("Weather:", current["weatherDesc"][0]["value"])
+print("Chance of Rain:", weather["hourly"][0]["chanceofrain"], "%")
